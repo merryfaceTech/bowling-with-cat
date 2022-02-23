b639097e6b4f0ec6f305cd3e5a53d9427767cee0
Tarik Merrlees <tarik.merrylees@madetech.com>
2022-02-23 16:29:23 +0000

    WIP solving test 9

--- bowling.rb
+++ bowling.rb
@@ -1,7 +1,24 @@
 class Bowling
 	def score(rolls)
 		return nil if rolls.nil?
+		total_score = 0
+		rolls_array = rolls.split(" ")
 
-		return rolls.split("").map(&:to_i).sum
+		rolls_array.map_with_index do |frame, index|
+			total_score += frame.split("").map(&:to_i).sum
+
+			# If it's a spare, make the roll total 10
+			if frame[1] == "/"
+				total_score += 10 - frame[0]
+			end
+
+			# If the previous roll is a spare, add the extra roll points
+			if index != 0 && rolls_array[index - 1][1]
+				total_score += frame[0]
+			end
+
+			return total_score
+		end
 	end
 end
+
